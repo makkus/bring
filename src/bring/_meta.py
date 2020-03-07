c0fec8bb4fa663d913448bfdabf8fa839eab36f0
Markus Binsteiner <markus@frkl.io>
2020-03-07 17:17:04 +0100

    ci: build fix

--- src/bring/_meta.py
+++ src/bring/_meta.py
@@ -3,5 +3,7 @@ from bring.defaults import BRINGISTRY_CONFIG
 from frtls.types.utils import load_modules
 
 
+app_name = "bring"
+
 _hi = load_modules(BRINGISTRY_CONFIG["modules"])  # type: ignore
 hiddenimports = [x.__name__ for x in _hi]
