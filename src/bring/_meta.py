42f2f3f8fba56f55d2dc51fbbc4b11322be8e638
Markus Binsteiner <markus@frkl.io>
2020-03-07 21:18:18 +0100

    chore: pkg metadata

--- src/bring/_meta.py
+++ src/bring/_meta.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from bring.defaults import BRINGISTRY_CONFIG
+from bring.defaults import BRINGISTRY_CONFIG as _bc
 from frtls.types.utils import load_modules
 
 
 app_name = "bring"
 
-_hi = load_modules(BRINGISTRY_CONFIG["modules"])  # type: ignore
-hiddenimports = [x.__name__ for x in _hi]
+_hi = load_modules(_bc["modules"])  # type: ignore
+pyinstaller = {"hiddenimports": [x.__name__ for x in _hi]}
