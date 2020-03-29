953b4edfc1101715aece737db664b0f9dd0ab7c8
Markus Binsteiner <markus@frkl.io>
2020-03-29 21:51:28 +0200

    build: hidden imports for windows

--- src/bring/_meta.py
+++ src/bring/_meta.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+
 from bring.defaults import BRINGISTRY_PRELOAD_MODULES
 from frtls.types.utils import load_modules
 
@@ -6,4 +8,14 @@ from frtls.types.utils import load_modules
 app_name = "bring"
 
 _hi = load_modules(BRINGISTRY_PRELOAD_MODULES)  # type: ignore
+
 pyinstaller = {"hiddenimports": [x.__name__ for x in _hi]}
+if os.name != "nt":
+    import pkgutil
+    import jinxed.terminfo
+
+    _additional_hidden_imports = [
+        mod.name
+        for mod in pkgutil.iter_modules(jinxed.terminfo.__path__, "jinxed.terminfo.")
+    ]
+    pyinstaller["hiddenimports"].extend(_additional_hidden_imports)
