898cc67756d61764a481092a0cbd5af627fabdb5
Markus Binsteiner <markus@frkl.io>
2020-03-31 15:18:48 +0200

    ci: build Docker image

---
+++ docker/bring-update-context.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+bring self update
+bring export-context . -o binaries.bx
---
+++ docker/bring-update-repo.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+bring-update-context.sh
+
+git config --global user.name "Markus Binsteiner"
+git config --global user.email "markus@frkl.io"
+
+git add binaries.bx
+export NOW_TIMESTAMP=$(date --utc --iso-8601=seconds)
+git commit -m "chore: updated frecklets ($NOW_TIMESTAMP)"
+git push -o ci.skip "https://makkus:${CI_GIT_TOKEN}@${CI_REPOSITORY_URL#*@}" "HEAD:master"
