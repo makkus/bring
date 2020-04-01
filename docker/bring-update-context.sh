262345bb681e58f3e2be1ebf6de27c063f89991f
Markus Binsteiner <markus@frkl.io>
2020-04-01 13:55:20 +0200

    chore: add index name option

--- docker/bring-update-context.sh
+++ docker/bring-update-context.sh
@@ -1,4 +1,11 @@
 #! /usr/bin/env bash
 
+INDEX_NAME="${1}"
+
+if [ -z ${INDEX_NAME} ]
+then
+ INDEX_NAME=$(basename ${PWD})
+fi
+
 bring self update
-bring export-context . -o binaries.bx
+bring export-context . -o "${INDEX_NAME}.bx"
