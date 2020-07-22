f69099ce02e37a9f92b7edc28ac500bd5e27ed44
Markus Binsteiner <markus@frkl.io>
2020-07-22 18:45:05 +0200

    chore: remove jinxed dependency for windows

--- src/bring/_meta.py
+++ src/bring/_meta.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
-
 from bring.defaults import BRINGISTRY_PRELOAD_MODULES
 from frkl.common.types import load_modules
 
@@ -12,12 +10,12 @@ project_main_module = "bring"
 _hi = load_modules(BRINGISTRY_PRELOAD_MODULES)  # type: ignore
 
 pyinstaller = {"hiddenimports": [x.__name__ for x in _hi]}
-if os.name == "nt":
-    import pkgutil
-    import jinxed.terminfo
-
-    _additional_hidden_imports = [
-        mod.name
-        for mod in pkgutil.iter_modules(jinxed.terminfo.__path__, "jinxed.terminfo.")
-    ]
-    pyinstaller["hiddenimports"].extend(_additional_hidden_imports)
+# if os.name == "nt":
+#     import pkgutil
+#     import jinxed.terminfo
+#
+#     _additional_hidden_imports = [
+#         mod.name
+#         for mod in pkgutil.iter_modules(jinxed.terminfo.__path__, "jinxed.terminfo.")
+#     ]
+#     pyinstaller["hiddenimports"].extend(_additional_hidden_imports)
